5ff3fc7eb2a1d7f7d181960041c44f198aa968ea
Rami Osman <rami-ossma@live.com>
2017-08-02 08:46:45 +0200

    Initial commit

---
+++ app/src/main/java/securemessages/m5/com/securemessagespv/MainActivity.java
@@ -0,0 +1,13 @@
+package securemessages.m5.com.securemessagespv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
